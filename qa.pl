3fd59182bce892dac925de2a64f11c7eb234c9a5
r-best <bobbyisbest3@gmail.com>
2018-04-09 10:14:12 -0400

    Creating a fun recursive subject-finding method

--- qa.pl
+++ qa.pl
@@ -16,19 +16,35 @@ while(1){
     if($query =~ /^[Ee]xit$/){ last; }
     # Make sure query is formatted correctly
     elsif(!($query =~ /^[Ww](ho|hat|hen|here)\s+/)){ println "Please begin your query with a 'Who', 'What', 'When', or 'Where'"; next; }
-    
-    # Turn the W-word to lowercase to stop it from confusing the program later on
-    $query =~ s/^W/w/;
 
-    # Extract subject by looking for a string of capitalized words
-    my $subject = ($query =~ /(([A-Z][a-z]*\s?)+)/)[0];
-    chomp $subject;
+    my @split =~ ($query =~ /^([Ww](ho|hat|hen|here)\s+\w+)\s+(.*)/);
+    my $questionType = $split[0]; # "Who is", "When did", "Where is", etc.
+    my $subject = $split[2]; # The rest of the query that isn't part of the question type
 
     # Search Wikipedia for the subject
+    
+}
+
+# Tests if the given subject has a Wikipedia page, if not it
+# removes the last word and tries again recursively
+# Returns the substring of the subject that successfully
+# retrieved a Wiki page, or -1 if all words were removed
+# from the string and no Wiki page was found
+# Ex: If user gives query "When was George Washington born", 
+#       subject will be "George Washington born", and this 
+#       method will recurse down to find the "George Washington"
+#       page.
+sub testSubjectValid {
+    my $subject = $_[0];
     if(my $result = $wiki->search($subject)){
-        println $result->text();
+        return $subject;
     }
     else {
-        println $wiki->error();
+        if($subject =~ s/(.*)\s+\w+/\1/){
+            return testSubjectValid($subject);
+        }
+        else{
+            return -1;
+        }
     }
 }
