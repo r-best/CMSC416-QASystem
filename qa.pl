802cd9630ef2efb89fe58dd6d031b0878e6453cf
r-best <bobbyisbest3@gmail.com>
2018-04-09 21:44:30 -0400

    Improving testSubjectValid method and documentation

--- qa.pl
+++ qa.pl
@@ -8,48 +8,64 @@ my $wiki = WWW::Wikipedia->new();
 println "Please enter a question beginning with 'Who', 'What', 'When', or 'Where'";
 while(1){
     print "> ";
-    my $query = <>;
-    chomp $query;
-    $query =~ s/\?$//; # Remove question mark if the user included one
+    my $input = <>;
+    chomp $input;
+    $input =~ s/\?$//; # Remove question mark if the user included one
     
     # Exit condition
-    if($query =~ /^[Ee]xit$/){ last; }
-    # Make sure query is formatted correctly
-    elsif(!($query =~ /^[Ww](ho|hat|hen|here)\s+/)){ println "Please begin your query with a 'Who', 'What', 'When', or 'Where'"; next; }
+    if($input =~ /^[Ee]xit$/){ last; }
+    # Make sure input is formatted correctly
+    elsif(!($input =~ /^[Ww](ho|hat|hen|here)\s+/)){ println "Please begin your input with a 'Who', 'What', 'When', or 'Where'"; next; }
 
-    my @split = ($query =~ /^([Ww](ho|hat|hen|here)\s+\w+)\s+(.*)/);
-    my $questionType = $split[0]; # "Who is", "When did", "Where is", etc.
-    my $subject = $split[2]; # The rest of the query that isn't part of the question type
-
+    my @split = ($input =~ /^([Ww](?:ho|hat|hen|here)\s+\w+)\s+(.*)/);
+    
     # Search Wikipedia for the subject
-    my $reducedSubject = testSubjectValid($subject);
-    if($reducedSubject == -1){
+    my @reducedSubject = testSubjectValid($split[1]);
+    if($reducedSubject[0] == -1){
         println "I'm sorry, I can't find the answer to that question, feel free to try rephrasing it or asking another"; next;
     }
 
-    println $reducedSubject;
+    # Form an array with 3 elements:
+    #   [0] - Question type ('who is', 'when did', etc..)
+    #   [1] - Subject of question found by testSubjectValid()
+    #   [2] - Rest of query after the subject (or -1 if N/A)
+    my @question = ($split[0], $reducedSubject[0], $reducedSubject[1]);
+
+    println "C".$question[1];
 }
 
-# Tests if the given subject has a Wikipedia page, if not it
-# removes the last word and tries again recursively
-# Returns the substring of the subject that successfully
-# retrieved a Wiki page, or -1 if all words were removed
-# from the string and no Wiki page was found
-# Ex: If user gives query "When was George Washington born", 
-#       input will be "George Washington born", and the 
-#       method will recurse down to find the "George Washington"
-#       page.
+# Finds the subject of a query by recursively removing the last
+# word until it finds a substring that has a Wikipedia page
+# Input: The user's query minus the question type
+#   - Optional: the substring that has been removed from the end so far, 
+#       compounds as the recursive calls continue and gets returned
+#       at the end (see #2 in Returns)
+# Returns 3 items if a Wiki page is found (or (-1 -1 -1) if not):
+#   - The substring of the query that successfully
+#       retrieved a Wiki page
+#   - The rest of the query
+#   - The summary text of the Wiki page
+# Ex: If user gives input "When was George Washington born",
+#       input to function will be "George Washington born", and the
+#       function will recurse down to find the "George Washington" page,
+#       then return ["George Washington", *wiki page summary*]
 sub testSubjectValid {
     my $subject = $_[0];
+    my $ongoing = $_[1];
+    println "A".$subject."A";
+    println "A".$ongoing."A";
     if(my $result = $wiki->search($subject)){
-        return $subject;
+        println "RETURNING |".$subject."|"."$ongoing"."|";
+        return ($subject, $ongoing, $result->text());
     }
     else {
-        if($subject =~ s/(.*)\s+\w+/\1/){
-            return testSubjectValid($subject);
+        if(my @temp = ($subject =~ /(.*)\s+(\w+)/)){
+            println "\tB".$temp[0];
+            println "\tB".$temp[1];
+            return testSubjectValid($temp[0], $temp[1]." ".$ongoing);
         }
         else{
-            return -1;
+            return (-1, -1, -1);
         }
     }
 }
