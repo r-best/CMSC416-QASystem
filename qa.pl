60849423eac70ead70c22e2e278ffac3ac883581
r-best <bobbyisbest3@gmail.com>
2018-04-10 20:24:05 -0400

    Ngram extracting is done, just need to rank and tile them

--- qa.pl
+++ qa.pl
@@ -18,15 +18,34 @@ while(1){
     elsif(!($input =~ /^[Ww](ho|hat|hen|here)\s+/)){ println "Please begin your input with a 'Who', 'What', 'When', or 'Where'"; next; }
 
     # Split user's query into the question type ("who is", "what are", etc..) and the actual question
-    my ($questionType, $question) = ($input =~ /^([Ww](?:ho|hat|hen|here)\s+\w+)\s+(.*)/);
+    my ($questionType, $question) = ($input =~ /^([Ww](?:ho|hat|hen|here)\s+\w+(?:\s+a)?)\s+(.*)/);
     
     # Search Wikipedia for the subject, see testSubjectValid() method for details on return values
     my ($subject, $remainder, $wikiEntry) = testSubjectValid($question);
     if($subject[0] == -1){
         println "I'm sorry, I can't find the answer to that question, feel free to try another"; next;
     }
+    $wikiEntry =~ s/\(.*\)/\s/g;
+    # println $wikiEntry;
 
-    println "C".$question[1];
+    my @unigrams, @bigrams, @trigrams;
+    for my $transformed (transform($questionType, $subject, $remainder)){
+        my @matches = ($wikiEntry =~ /$transformed.*?[\.\?!]/sg);
+        for my $match (@matches){
+            my @tokens = split(/\s+/, $match);
+            for(my $i = 0; $i < 0+@tokens; $i++){
+                push @unigrams, $tokens[$i];
+                if($i > 0){
+                    push @bigrams, $tokens[$i-1]." ".$tokens[$i];
+                }
+                if($i > 1){
+                    push @trigrams, $tokens[$i-2]." ".$tokens[$i-1]." ".$tokens[$i];
+                }
+            }
+        }
+    }
+    println Dumper(@bigrams);
+    # println substr $wikiEntry, $index;
 }
 
 # Finds the subject of a query by recursively removing the last
@@ -48,6 +67,7 @@ sub testSubjectValid {
     my $subject = $_[0];
     my $ongoing = $_[1];
     if(my $result = $wiki->search($subject)){
+        $ongoing =~ s/(.*)\s+/\1/;
         return ($subject, $ongoing, $result->text());
     }
     else {
@@ -58,4 +78,28 @@ sub testSubjectValid {
             return (-1, -1, -1);
         }
     }
+}
+
+# Reformats the user's query into a variety of different
+# searchable forms (returns an array of them)
+# Inputs:
+#   - Question type ("who is", "what are", etc.)
+#   - Subject found by testSubjectValid())
+#   - Remainder of query (also returned from testSubjectValid())
+# Ex: "When was George Washington born?" => "George Washington was born"
+sub transform {
+    my ($questionType, $verb) = split(/\s+/, $_[0]);
+    my $subject = $_[1];
+    my @subjectSplit = split(/\s+/, $subject);
+    my $remainder = $_[2];
+
+    my @searches;
+
+    my $temp = "";
+    for(my $i = (0+@subjectSplit)-1; $i >= 0; $i--){
+        $temp = $subjectSplit[$i]." ".$temp;
+        push @searches, $temp.$verb." ".$remainder;
+    }
+    
+    return @searches;
 }
